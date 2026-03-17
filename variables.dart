0ce293a5573cbe0918f09c790c2ed73ad00414ea
Muhammad Rais <raismuhammd@gmail.com>
2026-03-17 11:17:13 +0700

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  final currenTime = DateTime.now();
+
+  int angka = 1;
+  double koma = 3.14;
+  double komaNol = 18.0;
+  double angkaBiasa = 1;
+
+  num koma2 = -12.12;
+
+  bool bisaMakan = false;
+
+  var sesuatu = "teks";
+
+  String makanan = "Nasi Goreng";
+
+  List listTakjil = ["Kue Basah", "Es Buah"];
+
+  print(listTakjil);
+}
